535f009e700113d70f6e2b8ef06ee590e64dee69
BuildTools <unconfigured@null.spigotmc.org>
2020-12-03 18:36:46 +0000

    Readying for heroku

--- installreq.bat
+++ installreq.bat
@@ -2,16 +2,6 @@
 
 :start
 cls
-
-echo setting python ver...
-set python_ver=39
-
-echo getting pip...
-python ./get-pip.py
-
-echo finding python...
-cd \
-cd \python%python_ver%\Scripts\
 echo installing modules...
 pip install discord.py
 pip install tweepy
